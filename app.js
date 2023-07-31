91c36766e00dc41f75dc492287f4413db9bd14bf
Doxstart <135213161+damidl1@users.noreply.github.com>
2023-07-31 13:13:00 +0200

    inizio task

---
+++ app.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+const inputUrl = process.argv[2];
+const splittedInputUrl = inputUrl.split('.');
+const ext = splittedInputUrl[splittedInputUrl.length -1];
+
+
+function readFile(url){
+    try {
+        const data = fs.readFileSync(url, "utf8");
+        return data;
+    } catch (err) {
+        console.error(err.message);
+        return null;
+    }
+}
+
+
+function writeData(url, data){
+    try {
+        fs.writeFileSync(url, data);
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+
+
+
