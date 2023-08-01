eb8d4297a9fb9064fb995faad2ea61b778d9b50a
Doxstart <135213161+damidl1@users.noreply.github.com>
2023-08-01 10:51:55 +0200

    risoluzione task

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
+ // una cosa utile da fare prima di iniziare un progetto node è usare npm init in terminale
+
+ 
 const fs = require("fs");
+ 
+ const fileUrl = process.argv[2]; // argomento 0 sempre node, argomento 1 nome della mia applicazione e da argomento 3 (che si prende con indice 2) in poi sono quelli che aggiungo e mi interessano, che di solito sono da indice 2 in poi
 
-const inputUrl = process.argv[2];
-const splittedInputUrl = inputUrl.split('.');
+ const outputUrl = createOutputUrl(fileUrl);
+
+ const data = readFile(fileUrl);  // andiamo a leggere il file
+
+ 
+ if (data) {  // se c'è data leggi il file con url e se c'è crea un file e riscrivilo ad es pippo-report
+    
+    const report = createReport(data);
+    writeData(outputUrl, report);
+ }
 
 
+
+ 
+ function createOutputUrl(url) { // prende url file input e fa url file output simile a quello di input ma con una differenza // esempio ./demo-files/pippo.txt => ./demo-files/pippo-report.txt
+   
+   const splittedUrl = url.split('.');
+  
+   const lastPart = splittedUrl.pop();  // togliamo via ultima parte dell'url
+   
+   const firstPart = splittedUrl.join('.');
+   
+   const finalUrl = firstPart + '-report.' + lastPart;
+
+   return finalUrl;
+ 
+}
+
+ 
+ 
 function readFile(url){
     try {
         const data = fs.readFileSync(url, "utf8");
@@ -24,5 +55,90 @@ function writeData(url, data){
 }
 
 
+function createReport(data) {  //scriverà il report partendo da data
+    
+    let report = 'numero di caratteri: ' + countChars(data) + '\n' + 
+                 'numero di parole: ' + countWords(data) + '\n' +
+                 'carattere più usato: ' + mostUsedChar(data) + '\n' +
+                 'parola con più occorrenze: ' + mostUsedWord(data);
+    return report;
+
+}
 
 
+function countChars(data) {
+    const dataWithoutSpaces = data.replace(/ /g, '');
+   
+   return dataWithoutSpaces.length;
+
+}
+
+
+function countWords(data) {
+    
+ const dataArray = data.split(' ');
+ return dataArray.length;
+
+
+}
+
+
+
+function mostUsedChar(data) {
+    
+    const dataWithoutSpaces = data.replace(/ /g, ''); // per rimuovere tutti gli spazi che ci sono (sia prima, che dopo che dentro le stringhe)
+
+    let charMap = {};
+
+    for (let i = 0; i < dataWithoutSpaces.length; i++) {
+        const char = dataWithoutSpaces[i];
+        
+        if (charMap[char]) {
+            charMap[char] += 1; // se la lettera c'è già aumenta il conto di 1
+        } else {
+            charMap[char] = 1;  // se la lettera non c'è ancora crea una nuova proprietà e quando incontra lettere già presenti le aggiunge
+        }
+    }
+  
+
+  const keyValues = Object.entries(charMap);
+  keyValues.sort((e1, e2) => {
+    const firstValue = e1[1];
+    const secondValue = e2[1];
+    return secondValue - firstValue;
+
+ });
+
+ return keyValues[0][0];  // prendiamo il primo elemento mettendo indice 0 e per prendere il secondo 
+ 
+
+}
+
+
+
+function mostUsedWord(data) {
+    
+    const splittedData = data.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()(\r\n|\n|\r)]/g,"").split(' ');
+    let wordMap = {};
+
+    for (let i = 0; i < splittedData.length; i++) {
+        const word = splittedData[i];
+
+        if (wordMap[word]) {
+            wordMap[word] += 1;
+        } else {
+            wordMap[word] = 1;
+        }
+        
+    }
+    const keyValues = Object.entries(wordMap);
+    keyValues.sort((e1, e2) => {
+      const firstValue = e1[1];
+      const secondValue = e2[1];
+      return secondValue - firstValue;
+  
+   });
+  
+   return keyValues[0][0];
+
+}
